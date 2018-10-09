a0f778d506d109f1e19d3c5a7f161e62500d86d6
kimbo9090 <tomas9080@gmail.com>
2018-10-09 14:40:31 +0200

    Añadido debuf y imagen

---
+++ app/src/main/java/com/tomas9080gmail/test_your_might/Constantes.java
@@ -0,0 +1,6 @@
+package com.tomas9080gmail.test_your_might;
+
+public class Constantes {
+    public static final String password = null;
+
+}
---
+++ app/src/main/java/com/tomas9080gmail/test_your_might/logoActivity.java
@@ -0,0 +1,50 @@
+package com.tomas9080gmail.test_your_might;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class logoActivity extends AppCompatActivity {
+    private String TAG = "logoActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        myLog.d(TAG,"Inicializando onCreate");
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        myLog.d(TAG,"Inicializando onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        myLog.d(TAG,"Inicializando onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        myLog.d(TAG,"Inicializando onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        myLog.d(TAG,"Inicializando onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        myLog.d(TAG,"Inicializando onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        myLog.d(TAG,"Inicializando onDestroy");
+        super.onDestroy();
+    }
+}
