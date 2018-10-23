edc8287061af0b4c1b38c7aa8af15c5b0ced8355
kimbo9090 <tomas9080@gmail.com>
2018-10-23 13:33:45 +0200

    Acercade arreglado, ahora funciona el back

--- app/src/main/java/com/tomas9080gmail/test_your_might/acercaDe.java
+++ app/src/main/java/com/tomas9080gmail/test_your_might/acercaDe.java
@@ -1,7 +1,10 @@
 package com.tomas9080gmail.test_your_might;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class acercaDe extends AppCompatActivity {
 
@@ -9,6 +12,26 @@ public class acercaDe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+            onBackPressed();
+            onNavigateUp();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
     @Override
     public void onBackPressed() {
