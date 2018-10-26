86b5a0b477cecf5efd1da4cb036090e649bea987
kimbo9090 <tomas9080@gmail.com>
2018-10-26 09:36:46 +0200

    Preguntas, Spinner y button añadido

--- app/src/main/java/com/tomas9080gmail/test_your_might/anadirTarea.java
+++ app/src/main/java/com/tomas9080gmail/test_your_might/anadirTarea.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class anadirTarea extends AppCompatActivity {
 
@@ -24,6 +26,10 @@ public class anadirTarea extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        //Rellenamos el spinner cuando se crea la pantalla
+        Spinner spinner = (Spinner) findViewById(R.id.spinner2);
+        String [] letra = {"Examen 1","Examen 2","Examen 3"};
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, letra));
     }
 
 }
--- app/src/main/java/com/tomas9080gmail/test_your_might/listadoPreguntas.java
+++ app/src/main/java/com/tomas9080gmail/test_your_might/listadoPreguntas.java
@@ -3,7 +3,6 @@ package com.tomas9080gmail.test_your_might;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -25,6 +24,8 @@ public class listadoPreguntas extends AppCompatActivity {
                 listadoPreguntas.this.startActivity(myIntent);
             }
         });
+
     }
+
 
 }
